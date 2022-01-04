035f67e6a156835deff68ef579f6a7b31fb9c66f
fahimtakin10 <95608267+fahimtakin10@users.noreply.github.com>
2022-01-05 00:31:17 +0600

    Update and rename location.html to location.php

--- public/location.php
+++ public/location.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="description" content="">
-  <meta name="keywords" content="">
-  <title></title>
-
-  <link rel="stylesheet" href="CSS/homepage.css">
-  <link rel="icon" href="">
-
-</head>
-
-
-<body>
-
-    <!----------------------------------Below Navbar----------------------------------->
 <div class="below-navbar">
 	<div class="gap text-center">
 	<h1>Visit Our Store</h1>
@@ -24,7 +7,7 @@
 	<h4>Come and get what you need!</h4>
 	</div>
 </div>
-	<!----------------------------------Map------------------------------->
+
   <div class="location">
     <div class="address">
       <address>
@@ -35,7 +18,9 @@
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d883.1152092761298!2d90.39472050375454!3d23.86425994122661!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c515923d01a9%3A0x3c33772c069fe07a!2sChinice%20varieties%20store!5e0!3m2!1sen!2sbd!4v1599266827019!5m2!1sen!2sbd" width="500" height="400" frameborder="0" style="border:0; margin: auto, auto;" aria-hidden="false" tabindex="0"></iframe>
     </div>
   </div>
-	<!----------------------------------Footer----------------------------------------->
+	
+
+
 
 </body>
 </html>
